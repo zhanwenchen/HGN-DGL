f4f72b445cbcb77e58b5eaafb7fcdff09d551222
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-09-10 14:59:05 -0400

    add linux instructions

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ conda install -c huggingface transformers
 
 # Install PyTorch
 conda install pytorch::pytorch torchvision torchaudio -c pytorch # Mac
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia # Ubuntu CUDA 11.8
 
 # Install SpaCy
 conda install -c conda-forge spacy
@@ -13,13 +14,19 @@ conda install -c conda-forge spacy
 python -m spacy download en_core_web_sm
 
 # Install DGL
-# conda install -c dglteam dgl
+conda install -c dglteam/label/cu118 dgl # CUDA
 pip install dgl -f https://data.dgl.ai/wheels/repo.html # Mac
 
 
 # Download HuggingFace model and properly install LFS to avoid error with Git LFS.
 cd models
-brew install git-lfs # Mac
+# Mac
+brew install git-lfs
+# 2. Linux
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+
+
 git lfs install
 git clone https://huggingface.co/distilbert-base-uncased # contains LFS
 cd distilbert-base-uncased
