6d3a69d82139f597af8aabe29cec05180ba2d2a4
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-09-10 14:44:05 -0400

    fix tuple error

--- para_select.py
+++ para_select.py
@@ -20,7 +20,7 @@ class ParagraphSelector(BertPreTrainedModel):
         super().__init__(config)
         self.config = config
         self.num_labels = config.num_labels
-
+        config.return_dict = False
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
